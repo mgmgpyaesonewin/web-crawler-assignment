2778391c9bb23b63c71267d7b037cad06f830fa1
Pyae Sone Win <pyaesone.win@wavemoney.com.mm>
2024-03-11 23:54:18 +0630

    test: 💍 added limitation testing

---
+++ application-frontend/__tests__/CsvFileInput.test.js
@@ -0,0 +1,42 @@
+import { render, screen, fireEvent, act } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import CsvFileInput from './../src/components/CsvFileInput'
+
+describe('CsvFileInput', () => {
+    test('renders file input and label', () => {
+        render(<CsvFileInput />)
+        const fileInput = screen.getByLabelText('Upload CSV file')
+        expect(fileInput).toBeInTheDocument()
+    })
+
+    test('should display error message if file type is not csv', () => {
+        render(<CsvFileInput />)
+        const fileInput = screen.getByLabelText('Upload CSV file')
+        fireEvent.change(fileInput, {
+            target: {
+                files: [
+                    new File(['(⌐□_□)'], 'chucknorris.png', {
+                        type: 'image/png',
+                    }),
+                ],
+            },
+        })
+
+        const errorMessage = screen.getByText('Only CSV files are allowed')
+        expect(errorMessage).toBeInTheDocument()
+    })
+
+    test('should display error message if keyword count is more than 100', async () => {
+        render(<CsvFileInput />)
+        const fileInput = screen.getByLabelText('Upload CSV file')
+
+        const keywords = Array.from({ length: 105 }, (_, i) => `keyword${i}`)
+        const file = new File([keywords], 'keywords.csv', { type: 'text/csv' })
+        await act(async () => {
+            fireEvent.change(fileInput, { target: { files: [file] } })
+        })
+
+        const errorMessage = screen.getByText('Only 100 keywords are allowed')
+        expect(errorMessage).toBeInTheDocument()
+    })
+})
